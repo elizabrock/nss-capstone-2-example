74ea4afe5bd25b99af49208e8bb4f43d0ab83a24
Eliza Brock <eliza@elizabrocksoftware.com>
2014-01-21 10:56:58 -0600

    Basic passing test, with grocerytracker executable

--- test/test_entering_purchases.rb
+++ test/test_entering_purchases.rb
@@ -1,7 +1,25 @@
-require 'minitest/autorun'
+require_relative 'helper'
 
 class TestEnteringPurchases < MiniTest::Unit::TestCase
-  def test_something
-    assert false
+  def test_valid_purchase_gets_saved
+    assert false, "Missing test implementation"
+  end
+
+  def test_invalid_purchase_doesnt_get_saved
+    assert false, "Missing test implementation"
+  end
+
+  def test_error_message_for_missing_price
+    assert false, "Missing test implementation"
+  end
+
+  def test_error_message_for_missing_calories
+    assert false, "Missing test implementation"
+  end
+
+  def test_error_message_for_missing_name
+    command = "./grocerytracker add"
+    expected = "You must provide the name of the product you are adding."
+    assert_command_output expected, command
   end
 end
