e9274c972fcc5a0539123d08c0b48b6037849431
Eliza Brock <eliza@elizabrocksoftware.com>
2014-01-21 11:56:53 -0600

    Final test staging data for tomorrow's sqlite

--- test/test_entering_purchases.rb
+++ test/test_entering_purchases.rb
@@ -1,6 +1,12 @@
 require_relative 'helper'
 
 class TestEnteringPurchases < MiniTest::Unit::TestCase
+  def test_valid_purchase_information_gets_printed
+    command = "./grocerytracker add Cheerios --calories 210 --price 1.50"
+    expected = "Theoretically creating: a purchase named Cheerios, with 210 calories and $1.50 cost"
+    assert_command_output expected, command
+  end
+
   def test_valid_purchase_gets_saved
     skip "needs implementation"
     assert false, "Missing test implementation"
