f1c6d42a5d146a8d4f2f15ac60da22304d51b193
Eliza Brock <eliza@elizabrocksoftware.com>
2014-01-21 11:29:07 -0600

    Finish implementing basic error messages for add.

--- test/test_entering_purchases.rb
+++ test/test_entering_purchases.rb
@@ -2,20 +2,33 @@ require_relative 'helper'
 
 class TestEnteringPurchases < MiniTest::Unit::TestCase
   def test_valid_purchase_gets_saved
+    skip "needs implementation"
     assert false, "Missing test implementation"
   end
 
   def test_invalid_purchase_doesnt_get_saved
+    skip "needs implementation"
     assert false, "Missing test implementation"
   end
 
   def test_error_message_for_missing_price
-    assert false, "Missing test implementation"
+    command = "./grocerytracker add Cheerios --calories 210"
+    expected = "You must provide the price of the product you are adding."
+    assert_command_output expected, command
+  end
+
+  def test_error_message_for_missing_calories_and_price
+    command = "./grocerytracker add Cheerios"
+    expected = "You must provide the price and total calories of the product you are adding."
+    assert_command_output expected, command
   end
 
   def test_error_message_for_missing_calories
-    assert false, "Missing test implementation"
+    command = "./grocerytracker add Cheerios --price 1.50"
+    expected = "You must provide the total calories of the product you are adding."
+    assert_command_output expected, command
   end
+
 
   def test_error_message_for_missing_name
     command = "./grocerytracker add"
